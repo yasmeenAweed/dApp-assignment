f5756c3dff57e2a9fa26d0e701caaf7de9f69456
yasmeenAweed <92175566+yasmeenAweed@users.noreply.github.com>
2021-12-22 17:40:19 +0300

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -4,31 +4,24 @@ pragma solidity ^0.8.0;
 
 contract Bank{
 
-    modifier onlyRegistered(){
+    mapping(address=>uint) account_balances;
 
-        require (false , "User not rigister, please rigister to use this method");
-        _;
+    
+    
+    function get_balance() external view  returns (uint){
+          return account_balances[msg.sender];
+
     }
-    function get_balance() external view onlyRegistered returns (uint){
-        return account_balances[msg.sender];
 
-    }
-
-    function transfer (uint amount, address acctToTransferTo) public onlyRegistered {
+    function transfer (address recipient , uint amount) public  {
 
      
      account_balances[msg.sender] -= amount;
-     account_balances[acctToTransferTo] += amount;
+     account_balances[recipient] += amount;
     }
    
 
-
-    function bankBalance() public view onlyRegistered returns (uint) { return address(this).balance;}
-
-    function account_balances() public view onlyRegistered returns (uint) { return account_balances[msg.sender];
-    }
-
-    function withdraw(uint amount) external {
+    function withdraw(uint amount) public {
 
         account_balances[msg.sender] -= amount;
         payable (msg.sender).transfer(amount);
@@ -44,45 +37,78 @@ contract Bank{
 
 contract FunBank{
 
-     uint number_of_accounts;
-     mapping (address=> uint) account_balances;
-     mapping(address=>uint) account_info_map;
+    uint number_of_accounts;
+
+    mapping(address=>uint) account_balances;
+    mapping(address=>uint) account_info_map;
 
     struct BankAccountRecord{
         uint account_number;
         string fullName;
-        string dateOfBirth;
         string profession;
-        string customer_addr;
+        string dateOfBirth;
         address wallet_addr;
+        string customer_addr;
     }
 
-    string public bank_name;
-    string public bank_address;
+    BankAccountRecord[]  bankAccountRecords;
+
+
+
+modifier onlyRegistered(){
+
+        require (account_info_map[msg.sender] > 0 , "User not rigister, please rigister to use this method");
+        _;
+    }
+
+    function get_balance() external view onlyRegistered returns (uint){
+          return account_balances[msg.sender];
+
+    }
+
+    function transfer (address recipient , uint amount) public onlyRegistered {
+
+     
+     account_balances[msg.sender] -= amount;
+     account_balances[recipient] += amount;
+    }
    
+
+    function withdraw(uint amount) public onlyRegistered {
+
+        account_balances[msg.sender] -= amount;
+        payable (msg.sender).transfer(amount);
+
+    }
+
+    receive() external payable{
+        account_balances[msg.sender] += msg.value;
+        
+    }
     
 
-    BankAccountRecord[]  bankAccountRecords;
-   
-    function rigister_account(string memory full_name_, 
-                              string memory profession_,
-                              string memory dateOfBirth_,
-                              string memory customer_addr_) external {
+    function rigister_account(
+        string memory fullName_,
+        string memory profession_,
+        string memory dateOfBirth_,
+        string memory customer_addr_) external {
 
 
         require(account_info_map[msg.sender] == 0 , "Account already exists!");
         
         
-        BankAccountRecord.push(
+        bankAccountRecords.push(
             BankAccountRecord({ 
             account_number:++number_of_accounts,
-            full_name:full_name_,
-            profession:dateOfBirth_,
+            fullName:fullName_,
+            profession:profession_,
             dateOfBirth:dateOfBirth_,
-            customer_addr:customer_addr_,
-            wallet_addr:msg.sender}));
+            wallet_addr:msg.sender,
+            customer_addr:customer_addr_}));
     
+        
+        
         account_info_map[msg.sender] = number_of_accounts;
-        return true;
+    
     }
-    }
+}
